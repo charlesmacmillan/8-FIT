112610b58b2b56fc17ff7cf95d6fd788839f1d80
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-08-27 18:07:15 -0600

    atlas hosting

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
+
+require('dotenv').config();
+
+require('./config/database');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/routines');

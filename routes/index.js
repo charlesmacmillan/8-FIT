cbef61729dba41dfba8f7a7250bfde40560fabad
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-08-30 22:36:42 -0600

    google oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.redirect('/users');
 });
+
+// Google OAuth - login
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+// Google OAuth - callback
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/users',
+    failureRedirect: '/users'
+  }
+));
+
+// Google OAuth - logout
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/users');
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 const methodOverride = require('method-override');
 
 require('dotenv').config();
 
 require('./config/database');
+require('./config/passport');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/routines');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -18,15 +21,26 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'iliketurtles',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

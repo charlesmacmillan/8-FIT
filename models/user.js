d3b6d121f59164540dd945378df3cb160d7885e1
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-08-31 10:41:06 -0600

    partials header and footer

--- models/user.js
+++ models/user.js
@@ -1,11 +1,25 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var userSchema = new mongoose.Schema(
+const exerciseSchema = {
+  exercise: String,
+  sets: Number,
+  reps: Number,
+  description: String
+}
+
+const workoutSchema = {
+  title: String,
+  description: String,
+  exercises: [exerciseSchema]
+}
+
+const userSchema = new mongoose.Schema(
   {
     name: String,
     email: String,
     avatar: String,
     googleId: String,
+    workouts: [workoutSchema]
   },
   {
     timestamps: true,
